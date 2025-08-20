c8b046f7c8ca6ae62df6e89e786eed7922eb8a44
Nagaraju-209 <dramarajudramaraju59@gmail.com>
2025-08-20 16:01:44 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -2,8 +2,4 @@
 set -e
 
 # Find container using port 5000 and stop it
-container_id=$(docker ps --filter "publish=5000" --format "{{.ID}}")
-if [ -n "$container_id" ]; then
-  echo "Stopping container using port 5000: $container_id"
-  docker stop "$container_id"
-fi
+echo "HI"
